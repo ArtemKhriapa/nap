3f2e210ebfca3e0de4d565be85e6aadc1078112e
Artem.H <art.pzt@gmail.com>
2018-07-06 17:42:14 +0300

    some fixes in tests

--- npp/apps/data/tests/test_data.py
+++ npp/apps/data/tests/test_data.py
@@ -13,6 +13,13 @@ class DataTest(TestCase):
         self.user = create_user('SomeTestUser')
         self.cat = Data.objects.create(text = self.test_text, user = self.user)
 
+    def test_get_home(self):
+        response = self.c.get(
+            '/api/home/'
+        )
+        # print (dump(response))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_get_data_without_login_user(self):
         response = self.c.get(
             '/api/data/'
@@ -44,6 +51,30 @@ class DataTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['text'], self.test_text*2)
         self.assertEqual(response.data['user'], self.user.id)
+
+    def test_post_clear_data_with_login_user(self):
+        login_user(self.c, self.user)
+        response = self.c.post(
+            '/api/data/',
+                data = {
+                    'text' : '',
+                    'user' : self.user.id
+                }
+        )
+        # print (dump(response))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['text'], ['This field may not be blank.'])
+
+        response = self.c.post(
+            '/api/data/',
+            data={
+                'text': self.test_text,
+                'user': '',
+            }
+        )
+        # print(dump(response))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['user'], ['This field may not be null.'])
 
     def test_post_data_without_login_user(self):
         response = self.c.post(
