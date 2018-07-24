726e935812f21838d5ac276da10a016a230a5308
Artem.H <art.pzt@gmail.com>
2018-07-24 22:42:05 +0300

    try google oauth

--- npp/npp/settings.py
+++ npp/npp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'social_django',
     'rest_framework',
 
     'apps.data',
@@ -74,10 +75,26 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect'
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'social_core.backends.github.GithubOAuth2',  # for Github authentication
+    'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'npp.wsgi.application'
 LOGIN_REDIRECT_URL=reverse_lazy('home')
--- npp/npp/urls.py
+++ npp/npp/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # url(r'^$', HomeView, name='home'),  # temporary plug
     url(r'^api/', include('api.urls')),  # all url, who work for REST
+    url(r'^auth/', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
 ]
